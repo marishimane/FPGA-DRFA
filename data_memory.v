828393c69ded7991fa3904a6696ab4e6b5ecac1f
koliao <francisco.roth8@gmail.com>
2023-06-24 16:13:08 -0300

    Add read_data register to data_memory module

--- data_memory.v
+++ data_memory.v
@@ -7,19 +7,23 @@ module data_memory(
 
   input [9:0] in_addr;
   input in_write_en;
-  input in_read_en;
 
   input [7:0] in_data;
   output [7:0] out_data;
 
   reg [7:0] mem [0:1023];
+  reg [7:0] read_data;
 
   initial begin
+      read_data <= 8'h00;
   end
 
+  // NOTA: secuencial y read_data disponible en mismo clock
+  // o combinacional y read_data disponible en siguiente clock
   always @(posedge clk) begin
-      if (in_write_en == 1'b1) mem[in_addr] <= in_data;
+      if (in_write_en == 1'b1) mem[in_addr] = in_data;
+      read_data = mem[in_addr];
   end
 
-  assign out_data = mem[in_addr];
+  assign out_data = read_data;
 endmodule
