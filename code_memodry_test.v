8673a50a94643967bfd730c7a17c9405373e3b99
koliao <francisco.roth8@gmail.com>
2023-06-24 15:36:57 -0300

    Code memory + tests and example memory

---
+++ code_memodry_test.v
@@ -0,0 +1,43 @@
+module code_memory_test;
+  reg clk = 0;
+  reg [8:0] in_addr;
+  reg [15:0] out_data;
+
+  code_memory CODE_MEMORY(
+    .clk(clk),
+    .in_addr(in_addr),
+    .out_data(out_data)
+  );
+
+  initial begin
+    // Dump waves
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+
+    // read first value
+    in_addr <= 9'b0_0000_0000;
+    toggle_clk;
+
+    if(out_data !== 16'b1111_0000_1111_0000) begin
+      $display("[TEST FAILED] 1");
+      $finish;
+    end
+
+    // read second value
+    in_addr <= 9'b0_0000_0001;
+    toggle_clk;
+
+    if(out_data !== 16'b0000_1111_0000_1111) begin
+      $display("[TEST FAILED] 2");
+      $finish;
+    end
+  end
+
+
+  task toggle_clk;
+    begin
+      #10 clk = ~clk;
+      #10 clk = ~clk;
+    end
+  endtask
+endmodule
---
+++ code_memory.v
@@ -0,0 +1,25 @@
+// Code gets loaded at synth time
+module code_memory(
+    clk,
+    in_addr, out_data, 
+);
+  input clk;
+
+  input [8:0] in_addr;
+
+  output [15:0] out_data;
+
+  reg [15:0] mem [0:511];
+  reg [15:0] read_data;
+
+  initial begin
+      // load code
+      $readmemh("code_memory_example.mem", mem);
+  end
+
+  always @(posedge clk) begin
+      read_data <= mem[in_addr];
+  end
+
+  assign out_data = read_data;
+endmodule
