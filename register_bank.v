bb9bb732a82ca0806c900afc8f70c4e846ccea8d
Luis Jose Martin <ljmartin@dc.uba.ar>
2023-06-25 18:41:37 -0300

    Add the 2 registers output for direct use in the ALU

--- register_bank.v
+++ register_bank.v
@@ -1,12 +1,13 @@
 module register_bank(
-  clk, in_selector, in_data,
-  write_en, read_en, out_data
+  clk, write_en, in_rx_selector, in_ry_selector,
+  in_data, out_rx_data, out_ry_data
 );
-  input clk, write_en, read_en;
-  input [2:0] in_selector;
+
+  input clk, write_en;
+  input [2:0] in_rx_selector, in_ry_selector;
   input [7:0] in_data;
 
-  output [7:0] out_data;
+  output [7:0] out_rx_data, out_ry_data;
 
   reg [7:0] registers [0:7];
 
@@ -18,9 +19,11 @@ module register_bank(
 
   always @(posedge clk) begin
     if ( write_en ) begin
-      registers[ in_selector ] <= in_data;
+      // Only one register can be written at a time
+      registers[in_rx_selector] <= in_data;
     end
   end
 
-  assign out_data = read_en? registers[ in_selector ] : 'bz;
+  assign out_rx_data = registers[in_rx_selector];
+  assign out_ry_data = registers[in_ry_selector];
 endmodule
--- register_bank_test.v
+++ register_bank_test.v
@@ -5,17 +5,18 @@
     end
 
 module test;
-  reg clk = 0, write_en = 0, read_en = 0;
-  reg [7:0] in_data, out_data;
-  reg [2:0] in_selector;
+  reg clk = 0, write_en = 0;
+  reg [7:0] in_data, out_rx_data, out_ry_data;
+  reg [2:0] in_rx_selector, in_ry_selector;
 
   register_bank registers(
     .clk(clk),
-    .in_selector(in_selector),
-    .in_data(in_data),
     .write_en(write_en),
-    .read_en(read_en),
-    .out_data(out_data)
+    .in_rx_selector(in_rx_selector),
+    .in_ry_selector(in_ry_selector),
+    .in_data(in_data),
+    .out_rx_data(out_rx_data),
+    .out_ry_data(out_ry_data)
   );
 
   initial begin
@@ -23,23 +24,20 @@ module test;
     $dumpfile("dump.vcd");
     $dumpvars(1);
 
+    // Write in Rx
     write_en <= 1;
     in_data <= 8'b10101010;
-    in_selector <= 3'b100;
+    in_rx_selector <= 3'b100;
     toggle_clk;
+    `assert( out_rx_data, 8'b10101010 );
 
-    in_data <= 8'b00001111;
-    in_selector <= 3'b000;
+    in_data <= 8'b11111111;
+    in_rx_selector <= 3'b001;
     toggle_clk;
+    `assert( out_rx_data, 8'b11111111 );
 
-    write_en <= 0;
-    read_en <= 1;
-    toggle_clk;
-    `assert( out_data, 8'b00001111 );
-
-    in_selector <= 3'b100;
-    toggle_clk;
-    `assert( out_data, 8'b10101010 );
+    in_ry_selector <= 3'b001;
+    `assert( out_ry_data, 8'b11111111 );
 
     toggle_clk;
   end
