dc3190f6b855d95f5fd8dbc7c8a71985f0affd0f
Mariana Shimane <mnshimane@hotmail.com>
2023-06-27 21:53:00 -0300

    Fix memory addresses on inverted io asm example

--- ejemplos_asm/inverted_io.asm
+++ ejemplos_asm/inverted_io.asm
@@ -4,7 +4,7 @@ init:
 	setr r1, 0x01 ; cte 1
 	selmb 11b     ; seteo el memory bank
 prog:
-	readm 0xFFFE  ; leo del puerto de entrada y lo guardo en R0
+	readm 0xFE    ; leo del puerto de entrada y lo guardo en R0
 	not r0        ;
 	add r0, r1    ; calculo el inverso aditivo y se guarda en R0
-	writem 0xFFFF ; escribo en el puerto de salida
+	writem 0xFF   ; escribo en el puerto de salida
