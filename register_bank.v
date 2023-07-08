5c206b43f80e2a78caa9e41893fa42dc30f45061
koliao <francisco.roth8@gmail.com>
2023-07-08 15:47:19 -0300

    Add indirect mode to register bank

--- register_bank.v
+++ register_bank.v
@@ -1,11 +1,12 @@
 module register_bank(
   clk, read_en, write_en, in_rx_selector, in_ry_selector,
+  in_indirect_mode_en,
   in_data, out_bus_data, out_rx_data, out_ry_data
 );
 
-  // TODO: agregar flag de "indirecto" para el writeToMemory indirecto a registro
   input clk, read_en, write_en;
   input [2:0] in_rx_selector, in_ry_selector;
+  input in_indirect_mode_en;
   input [7:0] in_data;
 
   output [7:0] out_bus_data, out_rx_data, out_ry_data;
@@ -25,7 +26,9 @@ module register_bank(
     end
   end
 
-  assign out_bus_data = read_en? registers[in_ry_selector] : 'bz;
+  assign out_bus_data = read_en
+    ? (in_indirect_mode_en? registers[registers[in_ry_selector]] : registers[in_ry_selector])
+    : 'bz;
   assign out_rx_data = registers[in_rx_selector];
   assign out_ry_data = registers[in_ry_selector];
 endmodule
--- register_bank_test.v
+++ register_bank_test.v
@@ -8,6 +8,7 @@ module test;
   reg clk = 0, read_en = 0, write_en = 0;
   reg [7:0] in_data, out_bus_data, out_rx_data, out_ry_data;
   reg [2:0] in_rx_selector, in_ry_selector;
+  reg in_indirect_mode_en = 0;
 
   register_bank registers(
     .clk(clk),
@@ -15,6 +16,7 @@ module test;
     .write_en(write_en),
     .in_rx_selector(in_rx_selector),
     .in_ry_selector(in_ry_selector),
+    .in_indirect_mode_en(in_indirect_mode_en),
     .in_data(in_data),
     .out_bus_data(out_bus_data),
     .out_rx_data(out_rx_data),
@@ -59,6 +61,38 @@ module test;
     in_ry_selector <= 3'b011;
     toggle_clk;
     `assert( out_bus_data, 8'b00000001 );
+    read_en <= 0;
+
+
+    // R0 <- 4
+    write_en <= 1;
+    read_en <= 0;
+    in_data <= 8'b00000100;
+    in_rx_selector <= 3'b000;
+    toggle_clk;
+    // R4 <- 8 
+    write_en <= 1;
+    read_en <= 0;
+    in_data <= 8'b00001000;
+    in_rx_selector <= 3'b100;
+    toggle_clk;
+
+    // without indirect mode
+    write_en <= 0;
+    read_en <= 1;
+    in_indirect_mode_en <= 0;
+    in_ry_selector <= 3'b000;
+    toggle_clk;
+    `assert( out_bus_data, 8'b00000100 );
+    read_en <= 0;
+
+    // with indirect mode
+    write_en <= 0;
+    read_en <= 1;
+    in_indirect_mode_en <= 1;
+    in_ry_selector <= 3'b000;
+    toggle_clk;
+    `assert( out_bus_data, 8'b00001000 );
     read_en <= 0;
   end
 
